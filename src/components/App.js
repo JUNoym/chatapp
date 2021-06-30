acd2528becaafd06858d70755f0623fb6143997d
JUNoym <junnyaooyama@gmail.com>
2021-06-30 16:15:19 +0900

    ✨ nameが空文字じゃなければ Mainコンポーネント切り替え

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import SignIn from './SignIn'
+import Main from './Main'
 
 export default () => {
   const [name, setName] = useState('')
-  return <SignIn setName={setName} />
+  // console.log(name);
+  // もしnameにsetNameでstringが渡された結果、文字列が代入されたらMainComponentを表示する
+  // 空文字だった場合はSingnInコンポーネントを表示する
+  if (name === '') {
+    return <SignIn setName={setName} />
+  } else {
+    return <Main name={name} />
+  }
 };
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Main = (name) => {
+    return (
+        <div>
+            チャット<br />
+            {name.name}
+        </div>
+    )
+}
+
+export default Main
